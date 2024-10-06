73bd207282407b2709f86ccbef5cdc48f50aec73
Maneesh Sutar <sutarmaneesh00@gmail.com>
2024-10-06 16:06:03 +0530

    Add created and modified date values in 'dates' config

--- quartz/components/Date.tsx
+++ quartz/components/Date.tsx
@@ -15,8 +15,7 @@ export function getDate(cfg: GlobalConfiguration, data: QuartzPluginData): Date 
       `Field 'defaultDateType' was not set in the configuration object of quartz.config.ts. See https://quartz.jzhao.xyz/configuration#general-configuration for more details.`,
     )
   }
-  const dateInput = data.frontmatter?.[cfg.defaultDateType] as string
-  return new globalThis.Date(dateInput)
+  return data.dates?.[cfg.defaultDateType]
 }
 
 export function formatDate(d: Date, locale: ValidLocale = "en-US"): string {
